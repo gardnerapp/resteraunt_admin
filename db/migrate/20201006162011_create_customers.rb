2f91b2a49acf42022ed12c2231b5c7bc75264719
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2020-10-06 12:44:15 -0400

    Customer Scaffold

---
+++ db/migrate/20201006162011_create_customers.rb
@@ -0,0 +1,13 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.string :address
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
